07f721a12676e20c8617f5c00ea7f878cfc4e0a3
Christian Moltó <cmolto@azigrene.es>
2023-12-07 12:19:19 +0100

    init repoç

---
+++ src/main/java/com/example/backtesthub/BacktestHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.backtesthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacktestHubApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(BacktestHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backtesthub/BacktestHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backtesthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacktestHubApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
